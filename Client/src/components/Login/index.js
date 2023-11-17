6d7257eb8e5e5e87482c0745812c6fa80af8c7ba
nntthuong <thuong.nn204855@gmail.com>
2023-11-17 18:08:06 +0700

    update Login/index.js

--- Client/src/components/Login/index.js
+++ Client/src/components/Login/index.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import { Button, Row, Col } from 'antd';
+import { Row, Col, Button, Typography } from 'antd';
 import { FacebookOutlined, GoogleOutlined } from '@ant-design/icons';
+import {useNavigate } from 'react-router-dom';
+import firebase, { auth } from '../../firebase/config';
+// import { addDocument, generateKeywords } from '../../firebase/services';
 import './LoginPage.css'; 
+// import firebase {auth} from '';
+
+const { Title } = Typography;
+
+const fbProvider = new firebase.auth.FacebookAuthProvider();
+const ggProvider = new firebase.auth.GoogleAuthProvider();
 
 const LoginPage = () => {
-  const handleFacebookLogin = () => {
+  // const handleLogin = async (provider) => {
+  //   const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+  //   if (additionalUserInfo?.isNewUser) {
+  //     addDocument('users', {
+  //       displayName: user.displayName,
+  //       email: user.email,
+  //       photoURL: user.photoURL,
+  //       uid: user.uid,
+  //       providerId: additionalUserInfo.providerId,
+  //       keywords: generateKeywords(user.displayName?.toLowerCase()),
+  //     });
+  //   }
+  const navigate = useNavigate();
+  const handleFbLogin = () => {
+    auth.signInWithPopup(fbProvider);
+  }
   
-  };
-
-  const handleGoogleLogin = () => {
-
-  };
+  auth.onAuthStateChanged((user) => {
+     console.log({user});
+     if(user) {
+      navigate('/');
+     }
+  })
 
   return (
     <div className="login-page">     
@@ -21,8 +47,8 @@ const LoginPage = () => {
             type="primary"
             icon={<FacebookOutlined />}
             size="large"
-            onClick={handleFacebookLogin}
-            style={{ backgroundColor: '#1877F2', borderColor: '#1877F2', width: '100%', marginBottom: 10}}
+            onClick={handleFbLogin}
+            className="fbButton"
           >
             Đăng nhập bằng Facebook
           </Button>
@@ -30,8 +56,8 @@ const LoginPage = () => {
             type="danger"
             icon={<GoogleOutlined />}
             size="large"
-            onClick={handleGoogleLogin}
-            style={{ backgroundColor: 'white', borderColor: 'gray', width: '100%' }}
+            // onClick={() => handleLogin(ggProvider)}
+            className="ggButton"
           >
             Đăng nhập bằng Google
           </Button>
