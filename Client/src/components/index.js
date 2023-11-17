ad42ed79b5f359d59e6406b0b2c60a326bcdffaa
nntthuong <thuong.nn204855@gmail.com>
2023-11-17 17:58:35 +0700

    add index.js in components

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,70 +1,19 @@
 import React from 'react';
-import { Row, Col, Button, Typography } from 'antd';
-import { FacebookOutlined, GoogleOutlined } from '@ant-design/icons';
-import {useNavigate } from 'react-router-dom';
-import firebase, { auth } from '../../firebase/config';
-// import { addDocument, generateKeywords } from '../../firebase/services';
-import './LoginPage.css'; 
-// import firebase {auth} from '';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-const { Title } = Typography;
+import Login from './Login/Login';
+import ChatRoom from './ChatRoom';
 
-const fbProvider = new firebase.auth.FacebookAuthProvider();
-const ggProvider = new firebase.auth.GoogleAuthProvider();
-
-const LoginPage = () => {
-  // const handleLogin = async (provider) => {
-  //   const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
-
-  //   if (additionalUserInfo?.isNewUser) {
-  //     addDocument('users', {
-  //       displayName: user.displayName,
-  //       email: user.email,
-  //       photoURL: user.photoURL,
-  //       uid: user.uid,
-  //       providerId: additionalUserInfo.providerId,
-  //       keywords: generateKeywords(user.displayName?.toLowerCase()),
-  //     });
-  //   }
-  const navigate = useNavigate();
-  const handleFbLogin = () => {
-    auth.signInWithPopup(fbProvider);
-  }
-  
-  auth.onAuthStateChanged((user) => {
-     console.log({user});
-     if(user) {
-      navigate('/');
-     }
-  })
-
+const App = () => {
   return (
-    <div className="login-page">     
-      <h1 className="app-name">Four T</h1>
-      <Row>
-        <Col>
-          <Button
-            type="primary"
-            icon={<FacebookOutlined />}
-            size="large"
-            onClick={handleFbLogin}
-            className="fbButton"
-          >
-            Đăng nhập bằng Facebook
-          </Button>
-          <Button
-            type="danger"
-            icon={<GoogleOutlined />}
-            size="large"
-            // onClick={() => handleLogin(ggProvider)}
-            className="ggButton"
-          >
-            Đăng nhập bằng Google
-          </Button>
-        </Col>
-      </Row>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/chatroom" component={ChatRoom} />
+        <Route path="/" component={Login} />
+      </Switch>
+    </Router>
   );
 };
 
-export default LoginPage;
+ReactDOM.render(<App />, document.getElementById('root'));
