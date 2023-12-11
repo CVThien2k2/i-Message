897751813d322d626b6149c5826a64752c839379
Cao Van Thien <Caovanthien09102002@gmail.com>
2023-12-12 04:58:49 +0700

    Profile

--- Client/src/components/chat/Profile.jsx
+++ Client/src/components/chat/Profile.jsx
@@ -67,8 +67,179 @@ export default function Profile() {
   };
 
   return (
-    <>
-      <p>Vào code code giao diện profile đi</p>
-    </>
+    <section
+      className="vh-100 d-flex align-items-center justify-content-center"
+      style={{ backgroundColor: "#f4f5f7" }}
+    >
+      <Card className="w-100 h-100" style={{ borderRadius: ".5rem" }}>
+        <Row className="w-100 h-100">
+          <Col
+            md="4"
+            className="gradient-custom text-center text-white"
+            style={{
+              borderTopLeftRadius: ".5rem",
+              borderBottomLeftRadius: ".5rem",
+            }}
+          >
+            <div>
+              <div className="profile-image-container">
+                <img
+                  src={profileData.avatar}
+                  alt="Avatar"
+                  className="my-5 profile-image"
+                  style={{ width: "300px" }}
+                />
+                <label htmlFor="fileInput">
+                  <Button variant="primary" className="button-under-image">
+                    <Plus style={{ marginRight: "5px" }} />
+                  </Button>
+                </label>
+                <input
+                  id="fileInput"
+                  type="file"
+                  style={{ display: "none" }}
+                  onChange={handleFileInputChange}
+                />
+              </div>
+              <h5 className="h5-button-spacing" style={{ color: "black" }}>
+                {profileData.name}
+              </h5>
+            </div>
+          </Col>
+          <Col md="8">
+            <Card.Body className="p-4">
+              {editMode ? (
+                <Form>
+                  <Form.Group as={Row} className="mb-3">
+                    <Form.Label column sm="4">
+                      Email
+                    </Form.Label>
+                    <Col sm="8">
+                      <Form.Control
+                        type="email"
+                        name="email"
+                        value={profileData.email}
+                        onChange={handleChange}
+                      />
+                    </Col>
+                  </Form.Group>
+                  <Form.Group as={Row} className="mb-3">
+                    <Form.Label column sm="4">
+                      Phone
+                    </Form.Label>
+                    <Col sm="8">
+                      <Form.Control
+                        type="text"
+                        name="numberPhone"
+                        value={profileData.numberPhone}
+                        onChange={handleChange}
+                      />
+                    </Col>
+                  </Form.Group>
+                  <Form.Group as={Row} className="mb-3">
+                    <Form.Label column sm="4">
+                      Name
+                    </Form.Label>
+                    <Col sm="8">
+                      <Form.Control
+                        type="text"
+                        name="name"
+                        value={profileData.name}
+                        onChange={handleChange}
+                      />
+                    </Col>
+                  </Form.Group>
+                  <Form.Group as={Row} className="mb-3">
+                    <Form.Label column sm="4">
+                      Address
+                    </Form.Label>
+                    <Col sm="8">
+                      <Form.Control
+                        type="text"
+                        name="address"
+                        value={profileData.address}
+                        onChange={handleChange}
+                      />
+                    </Col>
+                  </Form.Group>
+                  <Form.Group as={Row} className="mb-3">
+                    <Form.Label column sm="4">
+                      Gender
+                    </Form.Label>
+                    <Col sm="8">
+                      <Form.Control
+                        as="select"
+                        name="gender"
+                        value={profileData.gender}
+                        onChange={handleChange}
+                      >
+                        <option>Male</option>
+                        <option>Female</option>
+                        <option>Other</option>
+                      </Form.Control>
+                    </Col>
+                  </Form.Group>
+                  <div className="text-center mt-4 mb-4">
+                    <Button variant="primary" onClick={handleSaveClick}>
+                      Save
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      className="ms-2"
+                      onClick={handleCancelClick}
+                    >
+                      Cancel
+                    </Button>
+                  </div>
+                </Form>
+              ) : (
+                <div>
+                  <Row className="mb-3">
+                    <Col sm="6">
+                      <h6>Email</h6>
+                      <p className="text-muted">{profileData.email}</p>
+                    </Col>
+                    <Col sm="6">
+                      <h6>Phone</h6>
+                      <p className="text-muted">{profileData.numberPhone}</p>
+                    </Col>
+                    <Col sm="6">
+                      <h6>Name</h6>
+                      <p className="text-muted">{profileData.name}</p>
+                    </Col>
+                    <Col sm="6">
+                      <h6>Address</h6>
+                      <p className="text-muted">{profileData.address}</p>
+                    </Col>
+                    <Col sm="6">
+                      <h6>Gender</h6>
+                      <p className="text-muted">{profileData.gender}</p>
+                    </Col>
+                  </Row>
+                  <div className="d-flex justify-content-start">
+                    <a href="#!" className="me-3">
+                      <i className="fab fa-facebook fa-lg"></i>
+                    </a>
+                    <a href="#!" className="me-3">
+                      <i className="fab fa-twitter fa-lg"></i>
+                    </a>
+                    <a href="#!" className="me-3">
+                      <i className="fab fa-instagram fa-lg"></i>
+                    </a>
+                  </div>
+                </div>
+              )}
+              {!editMode && (
+                <div className="text-center mb-4">
+                  <Button variant="primary" onClick={handleEditClick}>
+                    Edit
+                  </Button>
+                </div>
+              )}
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </section>
   );
 }
