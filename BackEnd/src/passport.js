7cb5d31bc3488ed7bfc879880be6cfcd966c6585
Cao Văn Thiện <caovanthien09102002@gmail.com>
2024-07-04 23:15:16 +0700

    Authentication with google

--- BackEnd/src/passport.js
+++ BackEnd/src/passport.js
@@ -6,7 +6,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/v1/auth/google/callback",
+      callbackURL: `${process.env.BASE_URL}/api/v1/auth/google/callback`,
     },
     function (accessToken, refreshToken, profile, cb) {
       return cb(null, profile);
