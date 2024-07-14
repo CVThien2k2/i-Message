1ee62c0823ce94d6531fab37483cb5ab42443f48
Cao Văn Thiện <caovanthien09102002@gmail.com>
2024-07-14 12:21:52 +0700

    update

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -2,7 +2,7 @@ const app = require("../src/app");
 const port = process.env.PORT || 3002;
 
 const server = app.listen(port, () => {
-  console.log("Server is running on port: " + port);
+  console.log("Server is running on port:" + port);
 });
 process.on("SIGINT", () => {
   server.close(() => {
